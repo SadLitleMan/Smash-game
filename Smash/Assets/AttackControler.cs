31f466349881bb8e1c90b9355b2385bf989dc841
SadLitleMan <43964778+SadLitleMan@users.noreply.github.com>
2018-11-21 00:11:16 +0100

    update

---
+++ Smash/Assets/AttackControler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackControler : MonoBehaviour {
+
+    public Animator animator;
+    //private bool attackHandLight=false;
+    //private bool attackHandHeavy=false;
+
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            animator.SetBool("attackLightHand", true);
+            //animator.SetBool("attackLightHand", false);
+        }
+
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            animator.SetBool("attackHeavyHand", true);
+            //animator.SetBool("attackHeavyHand", false);
+        }
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            animator.SetBool("attackHeavyLeg", true);
+            //animator.SetBool("attackHeavyHand", false);
+        }
+
+        if (Input.GetButtonDown("Submit"))
+        {
+            animator.SetBool("attackLightLeg", true);
+            //animator.SetBool("attackHeavyHand", false);
+        }
+
+    }
+
+    public void endLightattackHand()
+    {
+        animator.SetBool("attackLightHand", false);
+    }
+
+    public void endHeavyAttackHand()
+    {
+        animator.SetBool("attackHeavyHand", false);
+    }
+
+    public void endLightAttackLeg()
+    {
+        animator.SetBool("attackLightLeg", false);
+    }
+
+    public void endHeavyAttackLeg()
+    {
+        animator.SetBool("attackHeavyLeg", false);
+    }
+
+
+}
---
+++ Smash/Assets/playerControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour {
+
+    public float speed;
+    public float jumpforce;
+    private Rigidbody2D rb;
+    private Vector2 input;
+    public Animator animator;
+
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal")*speed, 0);
+        animator.SetFloat("Speed",input.x);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = input;
+    }
+}
+
+
